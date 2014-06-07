58b16b2025222eda8fe0527ca1be3c49be3bad81
dwladdimiroc <daniel.wladdimiro@gmail.com>
2014-06-07 14:33:52 -0400

    First push

---
+++ interface/chat.js
@@ -0,0 +1,26 @@
+﻿var puerto = 8008;
+var io = require('socket.io').listen(puerto);
+
+
+io.sockets.on('connection', function (socket) { // conexion
+	
+	socket.on('ejemplo3', function (data) {
+		console.log("Entro al "+ data.room);
+		socket.join(data.room);
+	});
+	
+	socket.on('salirRoom', function (data) {
+		  socket.leave(data.room);
+	});
+
+	socket.on('disconnect', function () {
+		console.log("Usuario desconectado");
+	});
+	
+	socket.on('broadcastDeCliente', function (data) {
+		console.log("Un usuario envió el mensaje: " + data.mensaje);
+		socket.broadcast.emit('alguienBroadcasteo', { texto:data.mensaje});
+		//socket.broadcast manda a todos
+	});
+});
+
